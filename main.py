b06f0fa754cc0accb391f6cae50bf4b7198424be
kietlwastaken <131593373+kietlwastaken@users.noreply.github.com>
2025-05-14 20:05:07 +0100

    a

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+import tkinter as tk
 
+def createBoard():
+    global board
+    board = [["R", "k", "B", "Q", "K", "B", "k", "R"],
+             ["P", "P", "P", "P", "P", "P", "P", "P"],
+             [" ", " ", " ", " ", " ", " ", " ", " "],
+             [" ", " ", " ", " ", " ", " ", " ", " "],
+             [" ", " ", " ", " ", " ", " ", " ", " "],
+             [" ", " ", " ", " ", " ", " ", " ", " "],
+             ["P", "P", "P", "P", "P", "P", "P", "P"],
+             ["R", "k", "B", "Q", "K", "B", "k", "R"]
+             ]
+    
+def pos(notation):
+    file_to_col = {'A': 0, 'B': 1, 'C': 2, 'D': 3,
+                   'E': 4, 'F': 5, 'G': 6, 'H': 7}
+    col = file_to_col[notation[0].upper()]
+    row = 8 - int(notation[1])  # Row 1 is bottom → index 7
+    return row, col
+
+
+def printBoard():
+    global board
+    for row in board:
+        print(row)
+
+createBoard()
+printBoard()
+print(board[pos("B3")[0]][pos("B3")[1]])
