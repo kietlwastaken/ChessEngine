84a277f5c39917e504ce8e8df0480c0c6d66a0e8
kietlwastaken <131593373+kietlwastaken@users.noreply.github.com>
2025-05-14 20:46:32 +0100

    Refactor chess board initialization and piece positioning methods

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
 import tkinter as tk
 
-def createBoard():
-    global board
-    board = [["R", "k", "B", "Q", "K", "B", "k", "R"],
-             ["P", "P", "P", "P", "P", "P", "P", "P"],
-             [" ", " ", " ", " ", " ", " ", " ", " "],
-             [" ", " ", " ", " ", " ", " ", " ", " "],
-             [" ", " ", " ", " ", " ", " ", " ", " "],
-             [" ", " ", " ", " ", " ", " ", " ", " "],
-             ["P", "P", "P", "P", "P", "P", "P", "P"],
-             ["R", "k", "B", "Q", "K", "B", "k", "R"]
-             ]
+class ChessBoard:
+
+    def __init__(self):
+        self.board = [["R", "k", "B", "Q", "K", "B", "k", "R"],
+                ["P", "P", "P", "P", "P", "P", "P", "P"],
+                [" ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " "],
+                [" ", " ", " ", " ", " ", " ", " ", " "],
+                ["P", "P", "P", "P", "P", "P", "P", "P"],
+                ["R", "k", "B", "Q", "K", "B", "k", "R"]
+                ]
+        self.file_to_col = {'A': 0, 'B': 1, 'C': 2, 'D': 3,
+                            'E': 4, 'F': 5, 'G': 6, 'H': 7
+                            }
     
-def pos(notation):
-    file_to_col = {'A': 0, 'B': 1, 'C': 2, 'D': 3,
-                   'E': 4, 'F': 5, 'G': 6, 'H': 7}
-    col = file_to_col[notation[0].upper()]
-    row = 8 - int(notation[1])  # Row 1 is bottom → index 7
-    return row, col
+    def __call__(self, notation=None):
+        if notation == None:
+            result = ""
+            for row in self.board:
+                result += " ".join(row) + "\n"
+            
+            return result.rstrip("\n")
+        else:
+            col = self.file_to_col[notation[0].upper()]
+            row = 8 - int(notation[1])
+            return (self.board[row][col])
+        
+    def set(self, notation, piece):
+        col = self.file_to_col[notation[0].upper()]
+        row = 8 - int(notation[1])
+        self.board[row][col] = piece
 
 
-def printBoard():
-    global board
-    for row in board:
-        print(row)
 
-createBoard()
-printBoard()
-print(board[pos("B3")[0]][pos("B3")[1]])
+board = ChessBoard()
+print(board())
